421bd7d583d7a414c0ab4173fd46d7379d6a7eef
2384320 <chdms1549@gmail.com>
2023-04-06 22:12:12 +0900

    [Feat] 1018 체스판 다시 칠하기
    add annotation

--- 01000-01999/1018.java
+++ 01000-01999/1018.java
@@ -24,10 +24,13 @@ public class Main {
             }
         }
 
+        // 체스판은 최소 크기가 8x8이므로 한 칸씩 옆으로 이동하도록 함.
         for (int i = 8; i <= n; i++) {
             for (int j = 8; j <= m; j++) {
+                // coloring의 count: 각 색이 첫번째라고 할 때, 칠해야하는 칸의 수
                 black = coloring(i, j, 0);
                 white = coloring(i, j, 1);
+                // 구했던 값들과 전에 구했던 result를 비교하여 최솟값 선별
                 if (black >= white && result > white) result = white;
                 else if (black <= white && result > black) result = black;
             }
@@ -43,8 +46,12 @@ public class Main {
         int check = bnw;
         int count = 0;
 
-        for (int i = n-8; i < n; i++) {
-            for (int j = m-8; j < m; j++) {
+        // n과 m을 기준으로 8x8로 자른 후 칠해야 하는 색 선별
+        for (int i = n - 8; i < n; i++) {
+            for (int j = m - 8; j < m; j++) {
+                // check == 1 -> 하얀색 칸인가?
+                // check == 0 -> 검은색 칸인가?
+                // check에 따라 저장된 값이 일치하는지 비교하고 그렇지 않으면 count++
                 if (check == 1 && arr[i][j] == 'W') check = 0;
                 else if (check == 0 && arr[i][j] == 'B') check = 1;
                 else {
