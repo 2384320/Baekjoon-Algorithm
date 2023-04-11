22b3e08a84f39c69be3f6d8f3896496ae90426c8
2384320 <chdms1549@gmail.com>
2023-04-11 14:14:25 +0900

    [Upload] 1932 정수 삼각형
    Upload 1932 정수 삼각형 code

---
+++ 01000-01999/1932.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[][] arr;
+    static Integer[][] dp;
+    static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+        dp = new Integer[n][n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < i + 1; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[n-1][i] = arr[n-1][i];
+        }
+
+        bw.write(fibo(0, 0) + "");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    public static int fibo(int depth, int idx) {
+        if (depth == n - 1) return dp[depth][idx];
+
+        if (dp[depth][idx] == null) {
+            dp[depth][idx] = Math.max(fibo(depth + 1, idx), fibo(depth + 1, idx + 1)) + arr[depth][idx];
+        }
+        return dp[depth][idx];
+    }
+}
