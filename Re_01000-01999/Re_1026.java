ec9c84ce963d2d5e7aaf28ae7a90df4255b6eba6
2384320 <chdms1549@gmail.com>
2023-04-18 23:41:40 +0900

    [Upload] Re_1026 보물
    Upload Re_1026 보물 code

---
+++ Re_01000-01999/Re_1026.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(a);
+
+        Integer[] b = new Integer[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(b, Comparator.reverseOrder());
+
+        int s = 0;
+        for (int i = 0; i < n; i++) {
+            s += a[i] * b[i];
+        }
+
+        bw.write(s+"");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
