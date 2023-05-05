24f7f564ecb95b1ef5299df455a860df8c818f89
2384320 <chdms1549@gmail.com>
2023-05-05 19:38:51 +0900

    [Upload] 11279 최대 힙

---
+++ 11000-11999/11279.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2){
+                return - Integer.compare(o1, o2);
+            }
+        });
+
+        while (n-- > 0) {
+            int x = Integer.parseInt(br.readLine());
+            if (x != 0) queue.add(x);
+            else {
+                if (queue.isEmpty()) bw.write(0 + "\n");
+                else bw.write(queue.poll() + "\n");
+            }
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
