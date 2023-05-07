91fb50ced8ad5ae32cb013bf9795b0ee438ce1cf
2384320 <chdms1549@gmail.com>
2023-05-07 21:56:42 +0900

    [Upload] 1655 가운데를 말해요

---
+++ 01000-01999/1655.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> maxPQ = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Integer> minPQ = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (maxPQ.size() == minPQ.size()) {
+                maxPQ.add(num);
+
+                if (!minPQ.isEmpty() && maxPQ.peek() > minPQ.peek()) {
+                    minPQ.add(maxPQ.poll());
+                    maxPQ.add(minPQ.poll());
+                }
+            } else {
+                minPQ.add(num);
+
+                if (maxPQ.peek() > minPQ.peek()) {
+                    minPQ.add(maxPQ.poll());
+                    maxPQ.add(minPQ.poll());
+                }
+            }
+            bw.write(maxPQ.peek() + "\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
