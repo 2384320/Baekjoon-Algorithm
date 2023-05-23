84f1c9192f82cbb7397bec0012e00e10b91a0b45
2384320 <chdms1549@gmail.com>
2023-05-23 22:13:30 +0900

    [Upload] 1237 정ㅋ벅ㅋ

---
+++ 01000-01999/1237.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("문제의 정답");
+        bw.flush();
+        //br.close();
+        bw.close();
+    }
+}
