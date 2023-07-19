997cbeb2df8f34523bf53c729a3807bf7130cb5c
2384320 <chdms1549@gmail.com>
2023-07-19 17:18:54 +0900

    refactor: Re_4153 직각삼각형

---
+++ Re_04000-04999/Re_4153.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[3];
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[0] = Integer.parseInt(st.nextToken());
+            arr[1] = Integer.parseInt(st.nextToken());
+            arr[2] = Integer.parseInt(st.nextToken());
+
+            if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0) break;
+            Arrays.sort(arr);
+            bw.write(is_triangle(arr) + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    private static String is_triangle(int[] arr) {
+        if (Math.pow(arr[0], 2) + Math.pow(arr[1], 2) == Math.pow(arr[2], 2)) return "right";
+        return "wrong";
+    }
+}
