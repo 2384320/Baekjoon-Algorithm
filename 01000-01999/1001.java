0d9390a99747b51cdd6247ac78af7c3e28005503
2384320 <chdms1549@gmail.com>
2023-08-05 23:03:07 +0900

    feat: 1001 A-B

--- 01000-01999/1001.java
+++ 01000-01999/1001.java
@@ -2,12 +2,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String args[]) {
-        int a, b;
-        
         Scanner sc = new Scanner(System.in);
+        int a, b;
         a = sc.nextInt();
         b = sc.nextInt();
-        
         System.out.println(a-b);
     }
 }
