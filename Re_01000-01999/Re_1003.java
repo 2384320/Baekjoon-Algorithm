b25a032261a0f0586f32c230e8c6e3d297410359
2384320 <chdms1549@gmail.com>
2023-08-09 21:46:37 +0900

    refactor: Re_1003 피보나치 함수

---
+++ Re_01000-01999/Re_1003.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    private static Integer[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+
+        dp = new Integer[41][2];
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            fibonacci(n);
+            bw.write(dp[n][0] + " " + dp[n][1] + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    private static Integer[] fibonacci(int n) {
+        if (dp[n][0] == null || dp[n][1] == null) {
+            dp[n][0] = fibonacci(n - 1)[0] + fibonacci(n - 2)[0];
+            dp[n][1] = fibonacci(n - 1)[1] + fibonacci(n - 2)[1];
+        }
+        return dp[n];
+    }
+}
