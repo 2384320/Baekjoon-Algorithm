cb7c25160828284aeb7b6531c98d11d8a9d3a433
2384320 <chdms1549@gmail.com>
2023-09-11 16:04:41 +0900

    refactor: Re_1927 최소 힙

---
+++ Re_01000-01999/Re_1927.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        while (n-- > 0) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (pq.size() == 0) bw.write(0 + "\n");
+                else bw.write(pq.poll() + "\n");
+            } else
+                pq.offer(x);
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
