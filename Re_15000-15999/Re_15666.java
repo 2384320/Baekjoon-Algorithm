d3263ff9f564d4c181397a5f3073ca411eb057e1
2384320 <chdms1549@gmail.com>
2023-10-12 00:07:10 +0900

    refactor: Re_15666 N과 M (12)

---
+++ Re_15000-15999/Re_15666.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int n, m;
+    private static int[] arr, result;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        result = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0, 0);
+
+        br.close();
+    }
+
+    private static void dfs(int idx, int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(result[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        int num = 0;
+        for (int i = idx; i < n; i++) {
+            if (num == arr[i]) continue;
+            result[depth] = arr[i];
+            dfs(i, depth + 1);
+            num = arr[i];
+        }
+    }
+}
