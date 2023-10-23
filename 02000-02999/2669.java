c30eff2f58a541b4fea9f2da2cd7702baedb107a
2384320 <chdms1549@gmail.com>
2023-10-23 23:03:40 +0900

    feat: 2669 직사각형 네개의 합집합의 면적 구하기

---
+++ 02000-02999/2669.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int answer = 0;
+        boolean[][] arr = new boolean[101][101];
+
+        for (int i = 0; i < 4; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            for (int j = x1; j < x2; j++) {
+                for (int k = y1; k < y2; k++) {
+                    if (arr[j][k]) continue;
+                    arr[j][k] = true;
+                    answer++;
+                }
+            }
+        }
+        System.out.println(answer);
+        br.close();
+    }
+}
