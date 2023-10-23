185c80fca37cecd350e2323af77f221fee320670
2384320 <chdms1549@gmail.com>
2023-10-23 23:04:35 +0900

    refactor: Re_11404 플로이드

---
+++ Re_11000-11999/Re_11404.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    arr[i][j] = 0;
+                    continue;
+                }
+                arr[i][j] = 100_000_000;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            arr[a][b] = Math.min(arr[a][b], c);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= n; k++) {
+                    arr[j][k] = Math.min(arr[j][k], arr[j][i] + arr[i][k]);
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (arr[i][j] == 100_000_000)
+                    System.out.print(0 + " ");
+                else
+                    System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        br.close();
+    }
+}
