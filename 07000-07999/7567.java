e59c387bebebe5c108a8c53447e0fb7d7a0dcfa7
2384320 <chdms1549@gmail.com>
2023-11-11 23:12:03 +0900

    feat: 7567 그릇

---
+++ 07000-07999/7567.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int answer = 10;
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1))
+                answer += 5;
+            else
+                answer += 10;
+        }
+        System.out.println(answer);
+        br.close();
+    }
+}
