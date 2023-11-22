7e997c0caf05abfaa6a47ce13d154a3614c81aad
2384320 <chdms1549@gmail.com>
2023-11-22 22:58:15 +0900

    fix: 2133 타일 채우기
    n = 1에 ArrayIndexOutOfBoundsException 나던 문제 수정

--- 02000-02999/2133.java
+++ 02000-02999/2133.java
@@ -7,13 +7,15 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[2] = 3;
 
-        for (int i = 4; i <= n; i+=2) {
-            dp[i] = dp[i - 2] * dp[2];
-            for (int j = i - 4; j >= 0; j-=2) {
-                dp[i] += dp[j] * 2;
+        if (n > 1) {
+            dp[0] = 1;
+            dp[2] = 3;
+            for (int i = 4; i <= n; i += 2) {
+                dp[i] = dp[i - 2] * dp[2];
+                for (int j = i - 4; j >= 0; j -= 2) {
+                    dp[i] += dp[j] * 2;
+                }
             }
         }
 
