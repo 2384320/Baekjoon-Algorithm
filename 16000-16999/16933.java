40f93f955652bac4d6d930132d6d1d2d2d5c6759
2384320 <chdms1549@gmail.com>
2023-12-03 22:28:16 +0900

    feat: 16933 벽 부수고 이동하기 3

---
+++ 16000-16999/16933.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Node {
+        int x, y, k, count;
+        Node(int x, int y, int k, int count) {
+            this.x = x;
+            this.y = y;
+            this.k = k;
+            this.count = count;
+        }
+    }
+
+    private static int n, m, k;
+    private static int[] dx = {0, 0, 1, -1};
+    private static int[] dy = {1, -1, 0, 0};
+    private static int[][] arr;
+    private static boolean[][][] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        arr = new int[n][m];
+        visited = new boolean[n][m][k + 1];
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        System.out.println(bfs());
+        br.close();
+    }
+
+    private static int bfs() {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(0, 0, k, 1));
+        visited[0][0][k] = true;
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            if (node.x == n - 1 && node.y == m - 1) return node.count;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = node.x + dx[i];
+                int ny = node.y + dy[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (arr[nx][ny] == 1 && node.k > 0 && !visited[nx][ny][node.k - 1]) {
+                    if (node.count % 2 == 0)
+                        queue.offer(new Node(node.x, node.y, node.k, node.count + 1));
+                    else {
+                        queue.offer(new Node(nx, ny, node.k - 1, node.count + 1));
+                        visited[nx][ny][node.k - 1] = true;
+                    }
+                }
+
+                if (visited[nx][ny][node.k] || arr[nx][ny] == 1) continue;
+                queue.offer(new Node(nx, ny, node.k, node.count + 1));
+                visited[nx][ny][node.k] = true;
+            }
+        }
+        return -1;
+    }
+}
