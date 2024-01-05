f9cb2298417dfaeb4afa7588bf5b1d13c87d1ea5
2384320 <chdms1549@gmail.com>
2024-01-05 22:10:24 +0900

    feat: Re_1789 수들의 합

---
+++ Re_01000-01999/Re_1789.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long s = Long.parseLong(br.readLine());
+        long sum = 0;
+        int count = 0;
+        long idx = 1;
+
+        while (sum <= s) {
+            count++;
+            sum += idx;
+            idx++;
+        }
+        System.out.println(count - 1);
+        br.close();
+    }
+}
