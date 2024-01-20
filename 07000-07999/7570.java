e291f54517bd0a45ba0312b73a7d30cb3ad34e5c
2384320 <chdms1549@gmail.com>
2024-01-20 23:55:13 +0900

    feat: 7570 줄 세우기

---
+++ 07000-07999/7570.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            dp[num] = dp[num - 1] + 1;
+        }
+
+        Arrays.sort(dp);
+        System.out.println(n - dp[n]);
+        br.close();
+    }
+}
