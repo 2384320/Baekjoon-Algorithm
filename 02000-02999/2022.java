25b66d6cdf1f629609d767fc32f3bdba86294dca
2384320 <chdms1549@gmail.com>
2024-02-09 22:35:28 +0900

    feat: 2022 사다리

---
+++ 02000-02999/2022.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;  
+import java.io.IOException;  
+import java.io.InputStreamReader;  
+import java.util.StringTokenizer;  
+  
+public class Main {  
+    public static void main(String[] args) throws IOException {  
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
+        StringTokenizer st = new StringTokenizer(br.readLine());  
+        double x = Double.parseDouble(st.nextToken());  
+        double y = Double.parseDouble(st.nextToken());  
+        double c = Double.parseDouble(st.nextToken());  
+  
+        double left = 0;  
+        double right = Math.min(x, y);  
+  
+        while (right - left >= 0.001) {  
+            double mid = (left + right) / 2;  
+            double h1 = Math.sqrt(Math.pow(x, 2) - Math.pow(mid, 2));  
+            double h2 = Math.sqrt(Math.pow(y, 2) - Math.pow(mid, 2));  
+            double ans = (h1 * h2) / (h1 + h2);  
+  
+            if (ans >= c)  
+                left = mid;  
+            else
+	            right = mid;  
+        }  
+        System.out.printf("%.3f%n", right);  
+        br.close();  
+    }  
+}
