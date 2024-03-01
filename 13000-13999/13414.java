ec35749bca914215f88cad8d89673c308eaf92d9
2384320 <chdms1549@gmail.com>
2024-03-02 00:16:01 +0900

    refactor: 13414 수강신청

--- 13000-13999/13414.java
+++ 13000-13999/13414.java
@@ -1,48 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.LinkedHashSet;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int w = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
         int l = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        int answer = 0;
-        int totalWeight = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            int a = Integer.parseInt(st.nextToken());
-
-            if (!queue.isEmpty()) {
-                while (true) {
-                    if (queue.size() == w)
-                        totalWeight -= queue.poll();
-                    else {
-                        if (totalWeight + a <= l) {
-                            queue.offer(a);
-                            totalWeight += a;
-                            answer++;
-                            break;
-                        } else {
-                            queue.offer(0);
-                            answer++;
-                        }
-                    }
-                }
-            } else {
-                queue.offer(a);
-                totalWeight += a;
-                answer++;
-            }
+        LinkedHashSet<String> set = new LinkedHashSet<>();
+        for (int i = 0; i < l; i++) {
+            String str = br.readLine();
+            if (set.contains(str))
+                set.remove(str);
+            set.add(str);
         }
-        System.out.println(answer + w);
+
+        int idx = 0;
+        for (String str : set) {
+            if (idx == k) break;
+            System.out.println(str);
+            idx++;
+        }
         br.close();
     }
 }
